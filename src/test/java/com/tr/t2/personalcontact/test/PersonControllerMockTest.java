9d9acfeb4b27f094547a7410c7dc0925ff7b6f10
alicankustemur <alican.kustemur@gmail.com>
2016-04-10 14:57:00 +0300

    add testgetperson

--- src/test/java/com/tr/t2/personalcontact/test/PersonControllerMockTest.java
+++ src/test/java/com/tr/t2/personalcontact/test/PersonControllerMockTest.java
@@ -1,6 +1,8 @@
 package com.tr.t2.personalcontact.test;
 
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -12,6 +14,7 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -51,6 +54,27 @@ public class PersonControllerMockTest
 
 		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"))
 				.andExpect(model().attribute("persons", hasSize(2)));
+	}
+
+	@Test
+	public void TestgetPersonWhenClickUpdateButton() throws Exception
+	{
+		Person person = getSimplePerson();
+		Mockito.when(service.getPerson(Matchers.anyString())).thenReturn(person);
+
+		mockMvc.perform(get("/updatePerson?id=5")).andExpect(status().isOk()).andExpect(view().name("index"))
+				.andExpect(model().attribute("person", hasProperty("id", is("1"))))
+				.andExpect(model().attribute("person", hasProperty("name", is("Ali Can"))))
+				.andExpect(model().attribute("person", hasProperty("surname", is("Kuştemur"))));
+	}
+
+	public Person getSimplePerson()
+	{
+		Person person = new Person();
+		person.setId("1");
+		person.setName("Ali Can");
+		person.setSurname("Kuştemur");
+		return person;
 	}
 
 }
